12703835d40a2f495cb72f2b0240edf379ce39c0
nafiz <nafiz0khan1@gmail.com>
2022-01-09 03:28:56 +0600

    user/admin route custom middleware

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,23 @@
 <?php
 
+use App\Http\Controllers\Is_Admin;
 use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', function () {
-    return view('home');
+
+//user route
+Route::group(['middleware'=>"check"],function(){
+
+    Route::get('/', function () {
+        return view('home');
+    })->name("index");
+    
+   
+
 });
+
+Route::get('/check',[Is_Admin::class,"is_admin"]);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
